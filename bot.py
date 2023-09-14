9c729415d13d905d2d1cc76a7d497b7a2d2ed630
Dmitry <106688620+DrSleep16@users.noreply.github.com>
2023-09-14 17:27:27 +0300

    разбил на функции

--- bot.py
+++ bot.py
@@ -20,14 +20,25 @@ class TelegramLogHandler(logging.Handler):
         self.bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
-def launch_the_bot(api_token, chat_id, bot):
+def initialize_bot_and_logger():
+    load_dotenv()
+    root_logger.setLevel(logging.ERROR)
+    tg_token = os.getenv('TG_TOKEN')
+    api_token = os.getenv('API_TOKEN')
+    tg_chat_id = os.getenv('TG_CHAT_ID')
+    bot = Bot(tg_token)
+    log_handler = TelegramLogHandler(bot, tg_chat_id)
+    root_logger.addHandler(log_handler)
+    return bot, tg_chat_id, api_token
+
+
+def run_main_bot_loop(api_token, chat_id, bot):
     headers = {"Authorization": f"Token {api_token}"}
     url = "https://dvmn.org/api/long_polling/"
     params = None
     while True:
         try:
-            response = requests.get(url, headers=headers,
-                                    params=params)
+            response = requests.get(url, headers=headers, params=params)
             response.raise_for_status()
         except requests.exceptions.ReadTimeout:
             continue
@@ -38,33 +49,31 @@ def launch_the_bot(api_token, chat_id, bot):
             continue
         review_result = response.json()
         if review_result["status"] == "found":
-            new_attempts = review_result["new_attempts"]
-            last_attempt_timestamp = review_result["last_attempt_timestamp"]
-            params = {"timestamp": last_attempt_timestamp}
-            lesson_title = new_attempts[0]['lesson_title']
-            lesson_url = new_attempts[0]['lesson_url']
-            is_negative = new_attempts[0]['is_negative']
-            if is_negative:
-                message = f'''
-                У вас проверили работу «{lesson_title}»
-                К сожалению, в работе нашлись ошибки.
-                Ссылка на работу: {lesson_url}
-                '''
-            else:
-                message = f'''
-                У вас проверили работу «{lesson_title}»
-                Преподавателю всё понравилось! Можно приступить к следующему уроку!
-                Ссылка на работу: {lesson_url}
-                '''
-            bot.send_message(chat_id,text=message)
+            process_review_result(review_result, bot, chat_id)
+
+
+def process_review_result(review_result, bot, chat_id):
+    new_attempts = review_result["new_attempts"]
+    last_attempt_timestamp = review_result["last_attempt_timestamp"]
+    params = {"timestamp": last_attempt_timestamp}
+    lesson_title = new_attempts[0]['lesson_title']
+    lesson_url = new_attempts[0]['lesson_url']
+    is_negative = new_attempts[0]['is_negative']
+    if is_negative:
+        message = f'''
+        У вас проверили работу «{lesson_title}»
+        К сожалению, в работе нашлись ошибки.
+        Ссылка на работу: {lesson_url}
+        '''
+    else:
+        message = f'''
+        У вас проверили работу «{lesson_title}»
+        Преподавателю всё понравилось! Можно приступить к следующему уроку!
+        Ссылка на работу: {lesson_url}
+        '''
+    bot.send_message(chat_id, text=message)
+
 
 if __name__ == '__main__':
-    load_dotenv()
-    root_logger.setLevel(logging.ERROR)
-    tg_token = os.getenv('TG_TOKEN')
-    api_token= os.getenv('API_TOKEN')
-    tg_chat_id = os.getenv('TG_CHAT_ID')
-    bot = Bot(tg_token)
-    log_handler = TelegramLogHandler(bot, tg_chat_id)
-    root_logger.addHandler(log_handler)
-    launch_the_bot(api_token, tg_chat_id, bot)
+    bot, tg_chat_id, api_token = initialize_bot_and_logger()
+    run_main_bot_loop(api_token, tg_chat_id, bot)
